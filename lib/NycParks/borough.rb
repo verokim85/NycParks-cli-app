cc8f48c527e6e7e6b4f025f5cd348bf40a25acd5
Veronica <vero.a.kim@gmail.com>
2018-04-12 22:52:16 -0400

    created collection of boro url

--- lib/NycParks/borough.rb
+++ lib/NycParks/borough.rb
@@ -21,12 +21,20 @@ class NycParks::Borough
   end
 
   def self.scrape_borough_parks
+    hash = {}
     doc = Nokogiri::HTML(open("https://www.nycgovparks.org/park-features/parks-list"))
-
-    park_names = doc.css("#boro-park-highlights a").each do |list_park| puts list_park.text
+    doc.css("#li_id>.nav-tabs a").each do |boro|
+      hash["#{boro.text}".split.join.to_sym] = "https://www.nycgovparks.org/park-features/parks-list" + boro.attribute("href").text
 
     end
-    park_names
+
+
+
+    # ji = doc.css("#boro-park-highlights a").each do |list_park| puts list_park.text
+    #   binding.pry
+    #
+    # end
+    # park_names
   end
 
 
